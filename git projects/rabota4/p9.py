f71d1a5eb426af7725289899e77be38b99c39f33
Vladimir Tkachev <90560402+firststage0@users.noreply.github.com>
2021-12-14 15:30:38 +0300

    Update p9.py

--- git projects/rabota4/p9.py
+++ git projects/rabota4/p9.py
@@ -1,5 +1,5 @@
 #-*- coding: utf-8 -*-
-s = input('Введите строку ')
-sim = input('Какой символ надо удалить ')
-a = s.replace(sim, '')
-print(a)
+son = input('Введите строку ')
+aga = input('Какой символ надо удалить ')
+d_b = son.replace(aga, '')
+print(d_b)
