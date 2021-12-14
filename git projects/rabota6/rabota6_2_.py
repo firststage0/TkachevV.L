8db24a7606afbf610bacb2b12ae0d52259b40af4
firststage0 <red_tk@mail.ru>
2021-12-14 17:26:03 +0300

    Оно работает

--- git projects/rabota6/rabota6_2_.py
+++ git projects/rabota6/rabota6_2_.py
@@ -7,22 +7,20 @@ from tkinter import scrolledtext
 def ven1():
     n = int(name1.get())
     x = 0
-    for i in range(1, n + 1):
-        if n >= x:
-            x = i ** 2
+    while x <= n:
+            x = x ** 2
             res1.insert(INSERT, x)
             res1.insert(INSERT, " ",)   
-            i = i + 1
+            x = x + 1
 
 def ven2():
-    n = int(name2.get())
-    i = 2
-    while i <= n:
-        i += 1
-        if n % i == 0:
-            res2.insert(INSERT, i)
-            res2.insert(INSERT, " ",)
-            break
+    namen = int(name2.get())
+    x = 2
+    while namen % x != 0:
+        x +=1
+    res2.insert(INSERT, x,)
+    res2.insert(INSERT, " ")
+           
 
 def ven3():
     n = int(name3.get())
@@ -39,12 +37,45 @@ def ven4():
     x = int(nameone4.get())
     y = int(nametwo4.get())
     i = 1
-    while x <= y:
+    while x < y:
         if x == y:
             break
         x += x/10
         i += 1
     res4.insert(INSERT, i)
+    res4.insert(INSERT, " ")
+
+def ven5():
+    n = int(name5.get())
+    i = int(restwo5.get())
+    if n != 0:
+        i += 1
+        restwo5.delete(1.0, 'end')
+        restwo5.insert(INSERT, i)    
+    else:
+        resone5.delete(1.0, 'end')
+        resone5.insert(INSERT, i)
+
+
+def ven6():
+    sum = int(nameone6.get())
+    dlinna = int(nametwo6.get())
+    n = int(name6.get())
+    if n != 0:
+        sum = sum + n
+        dlinna = dlinna + 1
+        nameone6.delete(0,'end')
+        nameone6.insert (INSERT, sum)
+        nametwo6.delete(0,'end')
+        nametwo6.insert(INSERT, dlinna)
+    else:
+        res6.delete(1.0, END)
+        res6.insert (INSERT,sum/dlinna)
+        nameone6.delete(0,'end')
+        nameone6.insert(0,"0")
+        nametwo6.delete(0,'end')
+        nametwo6.insert(0,"0")
+    name6.delete(0, tk.END) 
 
 def ven7():
     n = int(resone7.get())
@@ -65,7 +96,37 @@ def ven7():
         resone7.insert(0, tk.END)
         restwo7.delete(0, tk.END)
         restwo7.insert(0, tk.END)
-   
+    name6.delete(0, tk.END)
+
+def ven8():
+    nn = int(name8.get())
+    a = int(nameone8.get())
+    amax = int(nametwo8.get())
+    if nn != 0:
+        if (nn == a):
+            amax = amax + 1
+            nametwo8.delete(0,"end")
+            nametwo8.insert(0,amax)
+            if amax > int(name38.get()):
+                name38.delete(0,'end')
+                name38.insert(0, amax)
+        else: 
+            nameone8.delete(0,'end')
+            nameone8.insert(0, nn)
+            amax = 0
+            nametwo8.delete(0, "end")
+            nametwo8.insert(0, "0")
+    else:
+        adrmax=int(name38.get())
+        res8.delete(1.0, tk.END)
+        res8.insert(INSERT, adrmax)
+        nameone8.delete(0,'end')
+        nameone8.insert(0,"0")
+        nametwo8.delete(0,'end')
+        nametwo8.insert(0,"0")
+        name38.delete (0,'end')
+        name38.insert (0,'0')
+    name6.delete(0, tk.END)       
 
 win = tk.Tk() # Главное окно
 win.config(bg="#CBB8D7")
@@ -90,9 +151,15 @@ tab_control.add(tab4, text='ven4')
 tab5 = ttk.Frame(tab_control)
 tab_control.add(tab5, text='ven5')
 
+tab6 = ttk.Frame(tab_control)
+tab_control.add(tab6, text='ven6')
+
 
 tab7 = ttk.Frame(tab_control)
 tab_control.add(tab7, text='ven7')
+
+tab8 = ttk.Frame(tab_control)
+tab_control.add(tab8, text='ven8')
 
 
 tab_control.pack(expand=1, fill='both')
@@ -169,10 +236,24 @@ res4.insert(INSERT, 'Result = ')
 
 
 
-label = tk.Label(tab5, text="Программа не работает(я не понял как это написать :/ )").grid(row = 0, column = 0, stick = 'w')
+label = tk.Label(tab5, text="Программа не работает(все еще :/)").grid(row = 0, column = 0, stick = 'wesn')
 
+#6
+label = Label(tab6, text="Вводите число и нажимайте на кнопку ").grid(column=0, row=0)
+name6 = Entry(tab6)
+name6.grid(column=0, row=1)
+btn6 = Button(tab6, text="Тык-тык", command = ven6).grid(row=1, column=3)
+res6 = scrolledtext.ScrolledText(tab6, width=30, height=10)
+res6.grid(row=3, column=0)
+res6.insert(INSERT, 'Result: ')
+nameone6 = Entry(tab6)
+nameone6.grid(row = 3, column = 1)
+nametwo6 = Entry(tab6)
+nametwo6.grid(row = 4, column = 1)
+nameone6.insert(INSERT,'0')
+nametwo6.insert(INSERT,'0')
 
-
+#7
 
 label = tk.Label(tab7, text="Нужно ввести число, нажать на кнопку.(При вводе 0 программа завершается)")
 label.grid(row = 0, column = 0)
@@ -193,6 +274,30 @@ resone7.insert(INSERT,' 0 ')
 restwo7 = tk.Entry(tab7)
 restwo7.grid(row = 4, column = 1)
 restwo7.insert(INSERT,' 0 ')
+
+
+#8
+label = Label(tab8, text="Введите число").grid(row=0, column=0)
+name8 = Entry(tab8)
+name8.grid(row=1, column=0)
+btn8 = Button(tab8, text="Тык-тык", command=ven8).grid(row=1, column=2)
+res8 = scrolledtext.ScrolledText(tab8, width=30, height=10)
+res8.grid(row=3, column=0)
+res8.insert(INSERT, 'Result: ')
+
+nameone8 = Entry(tab8)
+#nameone8.grid(row=4, column=1)
+
+nametwo8 = Entry(tab8)
+#nametwo8.grid(row=5, column=1)
+
+name38 = Entry(tab8)
+#name38.grid(row=6, column=1)
+
+
+nameone8.insert (INSERT,'0')
+nametwo8.insert (INSERT,'0')
+name38.insert (INSERT,'0')
 
 
 win.mainloop()
